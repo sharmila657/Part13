a780ba19b722d3fbf03c7a6828e12a0ab7e2a9b7
sharmila657 <sharmilajirel8@gmail.com>
2024-05-30 18:20:51 +0545

    ex-13.9 validation with error handling.

--- blog-server/util/middleware.js
+++ blog-server/util/middleware.js
@@ -1,8 +1,19 @@
 
 const errorHandler = (error, req, res, next) => {
-    console.log(error.message)
-    res.status(500).json({ error: 'Internal server Error' })
-
-}
+    console.error(error.message);
+    if (error.name === "CastError") {
+      return res.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
+      return res.status(400).json({ error: error.message });
+    }else if (error.name ===  'JsonWebTokenError') {
+      return res.status(401).json({ error: error.message })
+    }else if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({
+        error: 'token expired'
+      })
+    }
+  
+    next(error);
+  };
 
 module.exports = {errorHandler}
