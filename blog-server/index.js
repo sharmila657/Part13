de5e257ca291add6b4bcb100d934d2f3a25f44ce
sharmila657 <sharmilajirel8@gmail.com>
2024-05-30 19:30:44 +0545

    ex-13.10 post route created for login feature.

--- blog-server/index.js
+++ blog-server/index.js
@@ -5,12 +5,14 @@ const cors = require('cors')
 const { errorHandler } = require("./util/middleware")
 const app = express()
 const userRouter = require('./contollers/users')
+const loginRouter = require('./contollers/login')
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogController)
 app.use(errorHandler)
 app.use('/api/users', userRouter)
+app.use("/api/login",loginRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
